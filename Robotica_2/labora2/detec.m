243b4d441afb8623fd10b6fd47a4811087d913bc
Cris Pat <pat19218@uvg.edu.gt>
2023-07-20 21:00:00 -0600

    ok

--- Robotica_2/labora2/detec.m
+++ Robotica_2/labora2/detec.m
@@ -7,7 +7,7 @@
 clear;clc;
 
 %% Cargar imagen
-A = imread('res1.jpeg');
+A = imread('res2.jpg');
 %figure;
 %imshow(A);
 
@@ -36,19 +36,62 @@ subplot(1,4,4);
     title('Amarillo');
 
 %% Negativos imfill
-BW1fill = imfill(BW1,'holes');
-BW2fill = imfill(BW2, 'holes');
-BW3fill = imfill(BW3, "holes");
+BW1fill = imfill(BW1, 8,'holes');
+BW2fill = imfill(BW2, 8, 'holes');
+BW3fill = imfill(BW3, 8, "holes");
 
 % figure;
 % subplot(1,2,1)
-% imshow(BW3fill)
+% imshow(BW1fill)
 % title('Filled Image')
 % subplot(1,2,2)
-% imshow(BW3)
+% imshow(BW1)
 
 %% Segmentacion
 
+% -------------------------------------------------------------------------
+%               rojo
+% -------------------------------------------------------------------------
+BW_out_red = BW1fill;
 
+% Filter image based on image properties.
+BW_out_red = bwpropfilt(BW_out_red,'Area',[40, 8047]);
 
+% Get properties.
+properties_red = regionprops(BW_out_red, {'Area', 'Eccentricity', 'EquivDiameter',...
+    'EulerNumber', 'MajorAxisLength', 'MinorAxisLength', 'Orientation',...
+    'Perimeter', 'Centroid'});
 
+% -------------------------------------------------------------------------
+%               morado
+% -------------------------------------------------------------------------
+BW_out_purple = BW2fill;
+
+% Filter image based on image properties.
+BW_out_purple = bwpropfilt(BW_out_purple,'Area',[1050, 8047]);
+
+% Get properties.
+properties_purple = regionprops(BW_out_purple, {'Area', 'Eccentricity', 'EquivDiameter',...
+    'EulerNumber', 'MajorAxisLength', 'MinorAxisLength', 'Orientation',...
+    'Perimeter', 'Centroid'});
+
+% -------------------------------------------------------------------------
+%               amarillo
+% -------------------------------------------------------------------------
+BW_out_yellow = BW3fill;
+
+% Filter image based on image properties.
+BW_out_yellow = bwpropfilt(BW_out_yellow,'Area',[100, 8047]);
+
+% Get properties.
+properties_yellow = regionprops(BW_out_yellow, {'Area', 'Eccentricity', 'EquivDiameter',...
+    'EulerNumber', 'MajorAxisLength', 'MinorAxisLength', 'Orientation',...
+    'Perimeter', 'Centroid'});
+
+%%
+try
+    isempty(properties_purple.Centroid)
+    disp(properties_purple.Centroid)
+catch   
+    disp('no area')
+end
