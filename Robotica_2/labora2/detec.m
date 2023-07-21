83b1c07ef3aab0aca007f0ba7242138417f62e70
Cris Pat <pat19218@uvg.edu.gt>
2023-07-21 17:28:01 -0600

    Matlab
    detecta si es o no la resistencia de 4.7 kΩ

--- Robotica_2/labora2/detec.m
+++ Robotica_2/labora2/detec.m
@@ -1,4 +1,6 @@
-%% Laboratorio 2 parte I
+%% ========================================================================
+%                         Laboratorio 2 parte I
+% =========================================================================
 %  Curso:   Robotica 2
 %  Nombre:  Cristhofer Patzán
 %  Carné:   19219
@@ -6,12 +8,18 @@
 % 
 clear;clc;
 
-%% Cargar imagen
-A = imread('res2.jpg');
+%% ========================================================================
+%                            Cargar imagen
+% =========================================================================
+
+A = imread('res4.jpeg');
 %figure;
 %imshow(A);
 
-%% Mascaras
+%% ========================================================================
+%                               Mascaras
+% =========================================================================
+
 % Primer mascara (rojo)
 [BW1,maskek_red] = mask_red(A);
 
@@ -35,7 +43,10 @@ subplot(1,4,4);
     imshow(maskek_yellow);
     title('Amarillo');
 
-%% Negativos imfill
+%% ========================================================================
+%                           Negativos imfill
+% =========================================================================
+
 BW1fill = imfill(BW1, 8,'holes');
 BW2fill = imfill(BW2, 8, 'holes');
 BW3fill = imfill(BW3, 8, "holes");
@@ -47,11 +58,12 @@ BW3fill = imfill(BW3, 8, "holes");
 % subplot(1,2,2)
 % imshow(BW1)
 
-%% Segmentacion
+%% ========================================================================
+%                               Segmentacion
+% =========================================================================
 
-% -------------------------------------------------------------------------
-%               rojo
-% -------------------------------------------------------------------------
+%------------------------------- rojo -------------------------------------
+
 BW_out_red = BW1fill;
 
 % Filter image based on image properties.
@@ -62,9 +74,8 @@ properties_red = regionprops(BW_out_red, {'Area', 'Eccentricity', 'EquivDiameter
     'EulerNumber', 'MajorAxisLength', 'MinorAxisLength', 'Orientation',...
     'Perimeter', 'Centroid'});
 
-% -------------------------------------------------------------------------
-%               morado
-% -------------------------------------------------------------------------
+%----------------------------- morado -------------------------------------
+
 BW_out_purple = BW2fill;
 
 % Filter image based on image properties.
@@ -75,23 +86,49 @@ properties_purple = regionprops(BW_out_purple, {'Area', 'Eccentricity', 'EquivDi
     'EulerNumber', 'MajorAxisLength', 'MinorAxisLength', 'Orientation',...
     'Perimeter', 'Centroid'});
 
-% -------------------------------------------------------------------------
-%               amarillo
-% -------------------------------------------------------------------------
+chage_siza = isempty(properties_purple);
+
+if chage_siza
+    %disp('si estaba vacio')
+    % Filter image based on image properties.
+    BW_out_purple = bwpropfilt(BW2fill,'Area',[107, 8047]);
+
+    % Get properties.
+    properties_purple = regionprops(BW_out_purple, {'Area', 'Eccentricity', 'EquivDiameter',...
+    'EulerNumber', 'MajorAxisLength', 'MinorAxisLength', 'Orientation',...
+    'Perimeter', 'Centroid'});
+
+    %disp(properties_purple)
+
+end
+
+%------------------------ amarillo ----------------------------------------
+
 BW_out_yellow = BW3fill;
 
 % Filter image based on image properties.
-BW_out_yellow = bwpropfilt(BW_out_yellow,'Area',[100, 8047]);
+BW_out_yellow = bwpropfilt(BW_out_yellow,'Area',[381, 8047]);
 
 % Get properties.
 properties_yellow = regionprops(BW_out_yellow, {'Area', 'Eccentricity', 'EquivDiameter',...
     'EulerNumber', 'MajorAxisLength', 'MinorAxisLength', 'Orientation',...
     'Perimeter', 'Centroid'});
 
-%%
+%% ========================================================================
+%                         Distancias absolutas
+% =========================================================================
+
 try
-    isempty(properties_purple.Centroid)
-    disp(properties_purple.Centroid)
+    yellow_purple_x = abs(properties_yellow.Centroid(1)-properties_purple.Centroid(1));
+    yellow_red_x = abs(properties_yellow.Centroid(1)-properties_red.Centroid(1));
+    
+    
+    if yellow_red_x > yellow_purple_x
+        disp('Si es una resistencia de 4.7 kΩ')
+    else
+        disp('No es una resistencia de 4.7 kΩ')
+    end
 catch   
-    disp('no area')
+    disp('No es una resistencia de 4.7 kΩ')
 end
+
