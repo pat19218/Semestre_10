3e07a487f9ebef731174e6626ab5fda39af4e96d
Cris Pat <pat19218@uvg.edu.gt>
2023-11-20 18:01:47 -0600

    out semester

--- Tesis/Toolbox Epileptic EEG Analysis/pruebas/VAT/pruebas_VAT.m
+++ Tesis/Toolbox Epileptic EEG Analysis/pruebas/VAT/pruebas_VAT.m
@@ -2,7 +2,7 @@ N1 = size(Features_result.CarIctal.VectorCaracteristicasIctal,1);
 N2 = size(Features_result.CarSano.VectorCaracteristicasSano,1);
 ind_aleatorio1 = randperm(N1);
 ind_aleatorio2 = randperm(N2);
-M = 100;
+M = 2300;
 
 general = [Features_result.CarIctal.VectorCaracteristicasIctal(ind_aleatorio1(1:M),:);...
            Features_result.CarSano.VectorCaracteristicasSano(ind_aleatorio2(1:M),:)];
